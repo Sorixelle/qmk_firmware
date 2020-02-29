8f8f334ab83937cd204206e889055802b02ffcfe
Ruby Iris Juric <ruby@srxl.me>
2020-02-29 16:24:02 +1100

    Fix build failure

--- keyboards/hs60/v2/hhkb/rules.mk
+++ keyboards/hs60/v2/hhkb/rules.mk
@@ -29,3 +29,5 @@ SRC =	keyboards/wilba_tech/wt_main.c \
 		drivers/issi/is31fl3733.c \
 		quantum/color.c \
 		drivers/arm/i2c_master.c
+
+CFLAGS += "-Wno-error=deprecated" # qmk ppl pls update chibios
